1877fdd1418e91cfb3ba2972652b1c006fe19a90
Goodluck Okechukwu <ebubeokechukwu13@gmail.com>
2025-12-04 13:42:06 +0100

    Hi

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Replace 'my-ts-app' with your actual GitHub repository name
-const repoName = '/My-Portfolio/';
+const repoName = '/My-Portfolio';
 
 export default defineConfig({
   plugins: [react()],
